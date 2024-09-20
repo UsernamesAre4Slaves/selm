bd9ce927cc6ec9ddd0456bd2f0fdbea5c66b7edc
MrE-The-Janitor <163067210+UsernamesAre4Slaves@users.noreply.github.com>
2024-09-20 07:15:06 -0500

    Delete frontend directory

--- frontend/src/axiosConfig.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Ensure this URL matches your backend server
-});
-
-export default instance;
