89703f294b9e9948ee4ff840852af13e5134ac48
MrE-The-Janitor <163067210+UsernamesAre4Slaves@users.noreply.github.com>
2024-09-20 07:15:53 -0500

    Delete scripts directory

--- scripts/neuro_evolution.py
+++
@@ -1,7 +0,0 @@
-"""
-High-level script for running neuro-evolutionary optimization.
-"""
-from src.neuro_evolution.neuro_evolution_engine import run_evolution
-
-if __name__ == "__main__":
-    run_evolution()
